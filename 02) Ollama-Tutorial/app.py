e51747ca1457253b6083a4724e92b7e21c761535
shaheer9023 <shaheerahmad9023@gmail.com>
2025-02-22 14:21:29 +0500

    Ai Agent

--- 02) Ollama-Tutorial/app.py
+++ 02) Ollama-Tutorial/app.py
@@ -1,8 +1,9 @@
 import ollama
-
+prompt=input("enter your prompt : ")
 Model = "llama3.2"  
 messages = [
-    {"role": "user", "content": "Describe AI"}
+    {"role": "user",
+     "content": prompt}
 ]
 
 response = ollama.chat(model=Model, messages=messages)
